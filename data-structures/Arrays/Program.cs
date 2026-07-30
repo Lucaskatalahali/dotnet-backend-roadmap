fdf481b6845bb602c9d944ef135a6527080c0d68
Lucas Isaac C. Katalahali <lucasick211@gmail.com>
2026-07-30 16:45:11 +0300

    Fixed namespace issue and did some testes

--- data-structures/Arrays/Program.cs
+++ data-structures/Arrays/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Arrays;
+
+
+Console.WriteLine("Hello, World!");
+
+ArrayList<int> intList = new ArrayList<int>(4);
+
+if(intList.IsEmpty()) 
+Console.WriteLine("Empty");
+    intList.Add(2);
+    
+    Console.WriteLine(intList.Contains(2));
+    Console.WriteLine(intList.IndexOf(2));
+    Console.WriteLine(intList.Capacity);
+    Console.WriteLine(intList.GetAt(0));
+    Console.WriteLine(intList.GetAt(1));
