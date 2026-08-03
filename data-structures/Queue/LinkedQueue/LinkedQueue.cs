d42690b4a5baeb307bd770e98ddf56ac07f1ba8c
Lucas Isaac C. Katalahali <lucasick211@gmail.com>
2026-08-03 11:34:13 +0300

    implementation of linked queue console app

---
+++ data-structures/Queue/LinkedQueue/LinkedQueue.cs
@@ -0,0 +1,81 @@
+namespace LinkedQueue;
+public class LinkedQueue<T>
+{
+    private Node<T>? _head;
+    private Node<T>? _tail;
+    private int _count;
+
+    public LinkedQueue()
+    {
+        _head = null;
+        _tail = null;
+        _count = 0;
+    }
+    public int Count => _count;
+    public bool IsEmpty => _count == 0;
+
+    public void Enqueue(T item)
+    {
+        if (IsEmpty)
+        {
+            _head = _tail = new Node<T>(item);
+        }
+        else
+        {
+           _tail!.Next = new Node<T>(item); 
+           _tail = _tail.Next;
+        }
+        _count++;
+    }
+
+    public T Dequeue()
+    {
+        if(IsEmpty)
+        throw new InvalidOperationException("Can not dequeue an empty queue.");
+
+        T removed = _head!.Data;
+        _head = _head!.Next;
+        _count--;
+
+        return removed;
+    }
+
+    public T Peek()
+    {
+        if(IsEmpty)
+            throw new InvalidOperationException("Can not peek an empty queue");
+        
+        return _head!.Data;
+    }
+
+    public void Clear()
+    {
+        _head = _tail = null;
+        _count = 0;
+    }
+
+    public void Print()
+    {
+        Node<T>? current = _head;
+        while(current is not null)
+        {
+            Console.Write(current.Data);
+            current = current.Next;
+            if(current is not null)
+                Console.Write(", ");
+        }
+            
+    }
+
+    private class Node<TNode> //I didn't use T as parametre because it's alreaby being used by external class
+    {
+        public TNode Data{get;}
+        public Node<TNode>? Next{get; set;}
+
+        public Node(TNode data, Node<TNode>? next = null)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+}
--- data-structures/Queue/LinkedQueue/Program.cs
+++ data-structures/Queue/LinkedQueue/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LinkedQueue;
+
+LinkedQueue<int> queue = new();
+
+Console.WriteLine(queue.Count);
+Console.WriteLine(queue.IsEmpty);
+queue.Print();
+queue.Enqueue(4);
+queue.Print();
+Console.WriteLine(queue.Peek());
+Console.WriteLine(queue.Dequeue());
+
